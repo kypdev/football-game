d53a57d81fcd0d33ab7af4e447bd395e9a06c1dd
kypdev <kyptdev@gmail.com>
2020-08-11 21:48:07 +0700

    Playername & second bg

--- assets/scripts/Playername.ts
+++ assets/scripts/Playername.ts
@@ -5,24 +5,42 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class NewClass extends cc.Component {
+  @property(cc.Node)
+  editBoxPlayername: cc.Node;
 
-    @property(cc.Label)
-    label: cc.Label = null;
+  @property(cc.Node)
+  savePlayernameBtn: cc.Node;
 
-    @property
-    text: string = 'hello';
+  // LIFE-CYCLE CALLBACKS:
 
-    // LIFE-CYCLE CALLBACKS:
+  // onLoad () {}
 
-    // onLoad () {}
+  start() {}
 
-    start () {
+  // update (dt) {}
 
+  onClickSavePlayername() {
+    // console.log('save playername');
+
+    let playername = this.editBoxPlayername
+      .getChildByName("playername")
+      .getComponent(cc.Label).string;
+
+    // console.log("name: " + pn);
+
+    const regex = /^[a-zA-Z0-9]+$/;
+
+    if (playername.match(regex)) {
+      console.log("go second sence");
+      localStorage.setItem("playername", playername);
+      cc.director.loadScene("ShootFootball");
+      console.log("playname: " + localStorage.getItem("playername"));
+    } else {
+      console.log("show alert not match eng and numberic");
     }
-
-    // update (dt) {}
+  }
 }
